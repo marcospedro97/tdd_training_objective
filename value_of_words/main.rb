014cf9abb8279df77be85004b64612514a50e760
marcos.trindade <marcos.trindade@objective.com.br>
2022-02-24 15:19:35 -0300

    value of words

--- value_of_words/main.rb
+++ value_of_words/main.rb
@@ -1,14 +1,32 @@
 # frozen_string_literal: true
+require 'prime'
+require_relative '../happy_numbers/happy_numbers'
 
 class Main
-  def self.call()
+  def self.call(word:, method: :exec)
+    Main.new(word: word).send(method)
   end
 
-  def initialize()
+  def initialize(word:)
+    @word = word
+    @arr = ('a'..'z').to_a + ('A'..'Z').to_a
   end
-
-  private
 
   def exec
+    @word.split('').map do |char|
+      @arr.find_index(char) + 1
+    end.sum
+  end
+
+  def prime
+    Prime.prime? exec
+  end
+
+  def multiple_of_three_or_five?
+    exec.modulo(3).zero? || exec.modulo(5).zero?
+  end
+
+  def is_happy?
+    HappyNumbers.call(exec)
   end
 end
--- value_of_words/spec/main_spec.rb
+++ value_of_words/spec/main_spec.rb
@@ -2,5 +2,32 @@ require 'spec_helper'
 require_relative '../main'
 
 RSpec.describe Main do
-  it ''
+  it 'should return value of word' do
+    expect(Main.call(word: 'aaa')).to eq(3)
+  end
+
+  it 'should return true if word is prime' do
+    expect(Main.call(word: 'aaa', method: :prime)).to eq(true)
+  end
+
+  it 'should return false if word is prime' do
+    expect(Main.call(word: 'aaaa', method: :prime)).to eq(false)
+  end
+
+  it 'should return true if word is multiple of three or five' do
+    expect(Main.call(word: 'aaa', method: :multiple_of_three_or_five?)).to eq(true)
+  end
+
+  it 'should return false if word is multiple of three or five' do
+    expect(Main.call(word: 'aaaa', method: :multiple_of_three_or_five?)).to eq(false)
+  end
+
+  it 'should return true if word value is a happy number' do
+    expect(Main.call(word: 'g', method: :is_happy?)).to eq(true)
+  end
+
+
+  it 'should return false if word value is a happy number' do
+    expect(Main.call(word: 'h', method: :is_happy?)).to eq(false)
+  end
 end
